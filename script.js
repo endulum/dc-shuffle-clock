72f795f0ae12eef2ed7dd65e5d86daaccc5221d4
endulum <71853916+endulum@users.noreply.github.com>
2023-04-27 14:47:41 -0700

    tidying

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 let intervalID;
 
 const sounds = [];
+sounds[0] = null;
 sounds[1] = new Audio('./assets/sms1.mp3');
 sounds[2] = new Audio('./assets/sms2.mp3');
 sounds[3] = new Audio('./assets/sms3.mp3');
 sounds[4] = new Audio('./assets/sms4.mp3');
 sounds[5] = new Audio('./assets/sms5.mp3');
 
+const biomes = [];
+biomes[0] = null;
+biomes[1] = 'https://dragcave.net/locations/5';
+biomes[2] = 'https://dragcave.net/locations/1';
+biomes[3] = 'https://dragcave.net/locations/2';
+biomes[4] = 'https://dragcave.net/locations/3';
+biomes[5] = 'https://dragcave.net/locations/4';
+biomes[6] = 'https://dragcave.net/locations/6';
+
 // preferences
 
-let isSoundOn = true;
-let soundChoice = sounds[1];
+let soundChoice = sounds[0];
+let biomeChoice = biomes[1];
 let soundVolume = 1;
-
-let isNotifyOn = false;
+let isNotifyOn = true;
 
 // clock functions
 
@@ -28,9 +37,15 @@ function countTime() {
 function checkTime(minute, second) {
     if (second % 10 == 0) {
         if (Notification.permission === 'granted' && isNotifyOn) {
-            const notif = new Notification('beep boop');
+            const notif = new Notification('Cave Shuffle Clock', {
+                body: `The next cave shuffle will occur in __ seconds.`,
+            });
+            if (biomeChoice) notif.addEventListener('click', e => {
+                window.open(biomeChoice, '_blank');
+                e.preventDefault();
+            });
         }
-        if (isSoundOn) soundChoice.play();
+        if (soundChoice) soundChoice.play();
     }
 }
 
@@ -56,14 +71,12 @@ function stop() {
 
 // sound functions
 
-function toggleSound() {
-    isSoundOn = isSoundOn ? false : true;
-}
-
 function changeSound(n) {
     soundChoice = sounds[n];
-    soundChoice.volume = soundVolume;
-    console.warn('Changed choice of sound to choice #' + n);
+    if (soundChoice) {
+        soundChoice.volume = soundVolume;
+        console.warn('Changed choice of sound to choice #' + n);
+    } else console.warn('Sound off');
 }
 
 function changeVolume(v) {
@@ -72,26 +85,38 @@ function changeVolume(v) {
     console.warn('Changed volume of sound to ' + v);
 }
 
-// notifications
+// notification functions
 
 function toggleNotify() {
     isNotifyOn = isNotifyOn ? false : true;
+    console.warn(isNotifyOn ? 'Turned notifications on' : 'Turned notifications off');
 }
 
-function ask() {
-    Notification.requestPermission().then(permission => handle(permission));
+function changeBiome(b) {
+    biomeChoice = biomes[b];
+    if (biomeChoice) console.warn('Changed choice of biome jump to choice #' + b);
+    else console.warn('Turned off biome jump');
+    
 }
 
 function handle(permission) {
     switch (permission) {
-        case 'default': console.warn('Request notification access?'); break;
+        case 'default': console.warn('Notification access unknown.'); break;
         case 'denied': console.warn('Notification access denied :-('); break;
         case 'granted': console.warn('Notification access granted :-)');
     }
 }
 
+// for console
+
+function ask() {
+    Notification.requestPermission().then(permission => handle(permission));
+}
+
+// init
+
 if (!typeof Notification) {
-    console.warn('Notifications are not supported')
+    console.warn('Notifications are not supported.')
 } else {
     handle(Notification.permission);
 }

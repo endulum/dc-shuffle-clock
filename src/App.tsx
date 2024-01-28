0b91b41a1daaca0e2cfdb1da534a5eb152669cfa
endulum <71853916+endulum@users.noreply.github.com>
2024-01-27 21:21:14 -0800

    replace expand btn with test btn for sound and notif

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState, useEffect, type ChangeEvent } from 'react'
 
 import Clock from './components/Clock.tsx'
-import Expandable from './components/Expandable.tsx'
+import Toggle from './components/Toggle.tsx'
 
 import {
   type Settings, settingsInitializer, type NotifSupport, notifSupportInitializer
 } from './types.ts'
+
+import PlaySvg from './assets/play.svg'
 
 const sounds = new Array(5).fill(0).map((_dummy, index) => `SMS Alert ${index + 1}`)
 
@@ -37,24 +39,28 @@ export default function App (): JSX.Element {
   }
 
   function handleAlert (isHourly: boolean): void {
-    if (settings.soundEnabled) {
-      const sound = new Audio(`./audio/${settings.soundSelect}.mp3`)
-      sound.volume = settings.soundVolume / 100
-      sound.play().catch((e) => { console.warn(e) })
-    }
-    if (settings.notifsEnabled && notifSupport === 'allowed') {
-      let notifText = ''
-      if (isHourly) { notifText = `The hourly shuffle will occur in about ${settings.delay} seconds.` } else notifText = `The next cave shuffle will occur in ${settings.delay} seconds.`
-      const notif = new Notification('Cave Shuffle Clock', { body: notifText })
+    if (settings.soundEnabled) playSound()
+    if (settings.notifsEnabled && notifSupport === 'allowed') doNotify(isHourly)
+  }
 
-      if (settings.biomeEnabled) {
-        notif.addEventListener('click', (e) => {
-          e.preventDefault()
-          const url = `https://dragcave.net/locations/${settings.biomeSelect}`
-          if (settings.biomeOpenType === 'tab') window.open(url, '_blank')
-          if (settings.biomeOpenType === 'window') window.open(url, '', 'width=900,height=500')
-        })
-      }
+  function playSound (): void {
+    const sound = new Audio(`./audio/${settings.soundSelect}.mp3`)
+    sound.volume = settings.soundVolume / 100
+    sound.play().catch((e) => { console.warn(e) })
+  }
+
+  function doNotify (isHourly: boolean): void {
+    let notifText = ''
+    if (isHourly) { notifText = `The hourly shuffle will occur in about ${settings.delay} seconds.` } else notifText = `The next cave shuffle will occur in ${settings.delay} seconds.`
+    const notif = new Notification('Cave Shuffle Clock', { body: notifText })
+
+    if (settings.biomeEnabled) {
+      notif.addEventListener('click', (e) => {
+        e.preventDefault()
+        const url = `https://dragcave.net/locations/${settings.biomeSelect}`
+        if (settings.biomeOpenType === 'tab') window.open(url, '_blank')
+        if (settings.biomeOpenType === 'window') window.open(url, '', 'width=900,height=500')
+      })
     }
   }
 
@@ -62,36 +68,38 @@ export default function App (): JSX.Element {
     <div className="app">
       <Clock onAlert={handleAlert} delay={settings.delay} />
       <main aria-label="clock settings">
-        <div className="row">
-          <label className="delay-label" htmlFor="delay">
-            Warn me
-            {' '}
-            <input
-              type="number"
-              value={settings.delay}
-              min="1"
-              max="60"
-              id="delay"
-              className="input-delay"
-              onChange={handleInputChange}
-              title="delay in seconds"
-            />
-            {' '}
-            seconds before each shuffle.
-          </label>
-        </div>
-
-        <Expandable
-          setting={{
-            name: 'Sound',
-            id: 'soundEnabled',
-            bool: settings.soundEnabled
-          }}
-          onInputChange={handleInputChange}
-        >
-          <label className="setting-body-label" htmlFor="soundSelect">
-            <span>Select Sound</span>
-            <span className="spacer" />
+        <label className="delay-label" htmlFor="delay">
+          Warn me
+          {' '}
+          <input
+            type="number"
+            value={settings.delay}
+            min="1"
+            max="60"
+            id="delay"
+            className="input-delay"
+            onChange={handleInputChange}
+            title="delay in seconds"
+          />
+          {' '}
+          seconds before each shuffle.
+        </label>
+        <div className="setting">
+          <button type="button" onClick={playSound} className='setting-test-button' title='Test Sound'>
+            <img src={PlaySvg} />
+          </button>
+          <Toggle
+            setting={{
+              name: 'Sound',
+              id: 'soundEnabled',
+              bool: settings.soundEnabled
+            }}
+            onInputChange={handleInputChange}
+          />
+          <div className="setting-body">
+            <label className="setting-body-label" htmlFor="soundSelect">
+              Select Sound
+            </label>
             <select className="setting-body-input" id="soundSelect" onChange={handleSelectChange} defaultValue={settings.soundSelect}>
               {sounds.map((sound) => (
                 <option key={sound} value={sound}>
@@ -99,29 +107,34 @@ export default function App (): JSX.Element {
                 </option>
               ))}
             </select>
-          </label>
 
-          <label className="setting-body-label" htmlFor="soundVolume">
-            <span>Sound Volume</span>
-            <span className="spacer" />
+            <label className="setting-body-label" htmlFor="soundVolume">
+              Sound Volume
+            </label>
             <input className="setting-body-input" type="range" id="soundVolume" onChange={handleInputChange} defaultValue={settings.soundVolume} />
-          </label>
-        </Expandable>
+          </div>
+        </div>
 
-        <Expandable
-          setting={{
-            name: 'Notifications',
-            id: 'notifsEnabled',
-            bool: settings.notifsEnabled,
-            notifSupport
-          }}
-          onInputChange={handleInputChange}
-          setNotifSupport={setNotifSupport}
-        >
-          <label htmlFor="biomeEnabled">
+        <div className="setting">
+          <button type="button" onClick={() => { doNotify(false) }} className='setting-test-button'>
+            <img src={PlaySvg} />
+          </button>
+          <Toggle
+            setting={{
+              name: 'Notifications',
+              id: 'notifsEnabled',
+              bool: settings.notifsEnabled,
+              notifSupport
+            }}
+            onInputChange={handleInputChange}
+            setNotifSupport={setNotifSupport}
+          />
+          <div className="setting-body">
             <input type="checkbox" id="biomeEnabled" onChange={handleInputChange} checked={settings.biomeEnabled} />
-            <span>
+
+            <label htmlFor="biomeEnabled">
               Jump to biome when clicked
+              <br />
               <small>
                 Take me to
                 {' '}
@@ -140,14 +153,14 @@ export default function App (): JSX.Element {
                   <option value="window">new window</option>
                 </select>
               </small>
-            </span>
-          </label>
+            </label>
 
-          <label htmlFor="notifAutoDismiss">
             <input type="checkbox" id="notifAutoDismiss" onChange={handleInputChange} checked={settings.notifAutoDismiss} />
-            <span>Automatically dismiss notification when shuffle occurs</span>
-          </label>
-        </Expandable>
+            <label htmlFor="notifAutoDismiss">
+              Automatically dismiss notification when shuffle occurs
+            </label>
+          </div>
+        </div>
       </main>
     </div>
   )

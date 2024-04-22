72391fa00cd6521f84ae8967c78947c43d5fa6db
endulum <71853916+endulum@users.noreply.github.com>
2024-04-22 13:46:19 -0700

    fix disabled settings still able to be tabbed to

--- src/components/settings-subcomponents/SettingBody.tsx
+++ src/components/settings-subcomponents/SettingBody.tsx
@@ -8,14 +8,23 @@ export default function SettingBody (
 ): JSX.Element {
   const bodyRef = useRef<HTMLDivElement | null>(null)
 
+  if (bodyRef.current !== null) {
+    bodyRef.current.querySelectorAll('input, select').forEach((child) => {
+      if (isVisible) child.removeAttribute('tabindex')
+      else child.setAttribute('tabindex', '-1')
+    })
+  }
+
   return (
     <div
       className="setting-body"
       ref={bodyRef}
+      aria-hidden={!isVisible}
       style={
         isVisible && bodyRef.current !== null
           ? {
-              height: bodyRef.current.scrollHeight
+              height: bodyRef.current.scrollHeight,
+              pointerEvents: 'none'
             }
           : { height: 0 }
       }
